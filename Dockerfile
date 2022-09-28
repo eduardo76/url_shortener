c846488661edc723ade2b70ad9f15cd5937a15a7
Natanael <natanaelsilva.progjs@gmail.com>
2022-09-28 00:32:46 -0300

    chose: setup

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Pull base image
+FROM python:3.7
+
+# Set environment varibles
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+COPY ./requirements.txt /app/
+RUN pip install -r requirements.txt
+
+COPY . /app/
+
+EXPOSE 8000
---
+++ alembic/versions/084b6c73706d_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 084b6c73706d
+Revises: 
+Create Date: 2022-09-28 03:27:20.136415
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '084b6c73706d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
