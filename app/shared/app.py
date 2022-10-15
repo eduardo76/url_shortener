fe67ff6441b9607a48bdcced0bb79bce7d960c1f
Natanael <natanaelsilva.progjs@gmail.com>
2022-10-15 18:58:15 -0300

    refactor: app

--- app/shared/app.py
+++ app/shared/app.py
@@ -1,17 +1,3 @@
-# import uvicorn
-# from fastapi import FastAPI
-
-
-# app = FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
 from fastapi import FastAPI
 
 from .routes import api
