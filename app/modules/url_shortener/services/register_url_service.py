97083c2853695e1aa4516b3193410f860161915c
Natanael <natanaelsilva.progjs@gmail.com>
2022-10-15 18:57:19 -0300

    refactor: url servece

--- app/modules/url_shortener/services/register_url_service.py
+++ app/modules/url_shortener/services/register_url_service.py
@@ -1,9 +1,11 @@
+import sys
+
 from typing import Type, Dict
 
 from app.modules.url_shortener.domain.repositories import UrlRepositoryInterface
 from app.modules.url_shortener.domain.models import RegisterUrlInterface
 from app.modules.url_shortener.domain.models import UrlDomain
-
+from app.shared.utils.utils import to_base62, generate_id
 
 class RegisterUrlService(RegisterUrlInterface):
     """
@@ -11,29 +13,53 @@ class RegisterUrlService(RegisterUrlInterface):
     """
 
     def __init__(self, url_repository: Type[UrlRepositoryInterface]):
+        """
+        Constructor
+
+        :param url_repository: Type[UrlRepositoryInterface]
+        """
         self.url_repository = url_repository
 
     
-    def register(self, data: UrlDomain) -> Dict[bool, UrlDomain]:
+    def register(self, url: str) -> Dict[bool, UrlDomain]:
         """
         Register URL
+
+        :param data: UrlDomain
+        :return: Dict[bool, UrlDomain]
         """
 
         try:
-            url_long = self.url_repository.get_by_long_url(data.long_url)
+            response = None
 
-            if url_long:
-                return {'success': False, 'data': url_long}
+            id = generate_id()
+            generated_hash = to_base62(id)
+            
+            data = {
+                "long_url": url,
+                "hash_url": generated_hash,
+                "status_url": "active",
+                "total_access": 0,
+            }   
+
+
+            response = self.url_repository.get_by_long_url(data['long_url'])
+
+            if response['success'] is True:
+                return {'success': True, 'data': response}
     
-            hash_url = self.url_repository.get_by_hash(data.hash_url)
+            # hash_url = self.url_repository.get_by_hash(data['hash_url'])
 
-            if hash_url:
-                return {'success': False, 'data': hash_url}
+            # if hash_url['success'] is True:
+            #     return {'success': False, 'data': hash_url}
 
             url = self.url_repository.create(data)
 
+
             return {'success': True, 'data': url}
-        except:
+        except Exception as e:
+            print('===== Error 02 on line {} ======'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+
             raise Exception('Error to register URL')
 
 
