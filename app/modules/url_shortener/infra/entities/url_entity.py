288b00f1d9efa2eac8663c3d50f09f7ee46664e9
Natanael <natanaelsilva.progjs@gmail.com>
2022-10-15 18:55:04 -0300

    refactor: fields id_url and updated_at

--- app/modules/url_shortener/infra/entities/url_entity.py
+++ app/modules/url_shortener/infra/entities/url_entity.py
@@ -14,22 +14,14 @@ class Status(enum.Enum):
 class UrlShortenerEntity(Base):
     __tablename__ = "url_shortener"
 
-    id_url = Column(Integer, primary_key=True, index=True)
+    id_url = Column(Integer, primary_key=True, autoincrement=True, index=True)
     long_url = Column(String, nullable=False, unique=True)
     hash_url = Column(String, nullable=False)
     status_url = Column(Enum(Status), nullable=False)
     total_access = Column(Integer, default=0)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), server_onupdate=func.now())
 
-    def __init__(self, id_url: int, long_url: str, hash_url: str, total_access: str, status_url: str, created_at: str, updated_at: str):
-        self.id_url = id_url
-        self.long_url = long_url
-        self.hash_url = hash_url
-        self.total_access = total_access
-        self.status_url = status_url
-        self.created_at = created_at
-        self.updated_at = updated_at
 
     def to_domain(self) -> UrlDomain:
         return UrlDomain(
@@ -48,7 +40,4 @@ class UrlShortenerEntity(Base):
     def __eq__(self, other):
         if not isinstance(other, UrlShortenerEntity):
             return False
-        return self.id_url == other.url_id
-
-    def __hash__(self):
-        return hash(self.id_url)
+        return self.id_url == other.id_url
