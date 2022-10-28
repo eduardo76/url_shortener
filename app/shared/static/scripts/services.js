daaacd4b339f5fc07d400ad5361030a3f74cae79
Natanael <natanaelsilva.progjs@gmail.com>
2022-10-27 21:37:45 -0300

    feat: creation function sendFormTotalClicks

--- app/shared/static/scripts/services.js
+++ app/shared/static/scripts/services.js
@@ -9,9 +9,9 @@ const sendFormShortenUrl = async () => {
 
     let formData = new FormData(form);
     const data = JSON.stringify(Object.fromEntries(formData));
-    const url = form.getAttribute("data-action-shorten");
+    const url = form?.getAttribute("data-action-shorten");
 
-    const method = form.getAttribute("method");
+    const method = form?.getAttribute("method");
     const inputUrl = document.getElementById("long-url");
 
     if (!Utils.validateUrl(inputUrl.value)) {
@@ -33,4 +33,31 @@ const sendFormShortenUrl = async () => {
   });
 };
 
+
+const sendFormTotalClicks = async () => {
+  const form = document.getElementById("form-total-clicks");
+  
+  form?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    
+    let formData = new FormData(form);
+
+    const data = JSON.stringify(Object.fromEntries(formData));
+    const method = form?.getAttribute("method");
+    const hash = data.split("/")[3].replace(/"}/g, "");
+    const newUrl = `/total-clicks-result/${hash}`;
+    const resultTotalClicks = document?.querySelector(".result-total-clicks");
+
+    let response = await Utils.fetchForm(newUrl, method);
+    let json = await response.json();
+
+    if (json.success) {
+      resultTotalClicks.style.display = "flex";
+      resultTotalClicks.querySelector(".result span").innerHTML = json.total_access;
+    }
+
+  });
+}
+
 sendFormShortenUrl();
+sendFormTotalClicks();
