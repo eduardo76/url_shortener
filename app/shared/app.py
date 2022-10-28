0c2f392f526a88b22a92ea0646d6e64602f865e3
Natanael <natanaelsilva.progjs@gmail.com>
2022-10-27 22:17:15 -0300

    chose: settings CORS

--- app/shared/app.py
+++ app/shared/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import api
 
@@ -7,6 +8,18 @@ app = FastAPI(
     openapi_url="/openapi.json",
     docs_url="/docs",
     redoc_url="/redoc",
+)
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
 )
 
 app.mount("/static", StaticFiles(directory="app/shared/static"), name="static")
